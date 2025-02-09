187c1fb60635d878944e7fc654d22cfb1532d3a9
David Coles <davidepcoles@gmail.com>
2025-02-09 03:24:32 +0000

    Updated to Vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Welcome to the Wedding of
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
